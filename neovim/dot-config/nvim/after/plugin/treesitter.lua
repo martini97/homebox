a587001ec65389dd4f4af52c7a7a2a22ee7a179a
Alessandro Martini <martini97@protonmail.ch>
2024-11-12 17:19:35 -0300

    chore(neovim): use logger for ts

--- neovim/dot-config/nvim/after/plugin/treesitter.lua
+++ neovim/dot-config/nvim/after/plugin/treesitter.lua
@@ -1,11 +1,12 @@
 local ignore_fts = { "netrw", "fzf" }
 
+local logger = require("vlog").new({ plugin = "plugin.treesitter", level = vim.log.levels.OFF })
+
 local function treesitter_start(ev)
 	local ft = ev.match
 	local ok, err = pcall(vim.treesitter.start)
 	if not ok and not vim.list_contains(ignore_fts, ft) then
-		local msg = ("treesitter: failed to start for filetype='%s', err: %s"):format(ft, err)
-		vim.notify_once(msg, vim.log.levels.ERROR)
+		logger:error("treesitter failed to start", { filetype = ft, error = err })
 	end
 end
 
