614da9d4ed6ca0b17348e3a40c5534641998a3dd
Alessandro Martini <martini97@protonmail.ch>
2024-11-12 12:38:32 -0300

    fix(fish): fix zup failing when tmux not runnning

--- fish/dot-config/fish/functions/zup.fish
+++ fish/dot-config/fish/functions/zup.fish
@@ -3,7 +3,9 @@ function zup
   set -l session_cwd (__zup_get_session_cwd $query)
   set -l session_name (__zup_get_session_name $session_cwd)
 
-  __zup_create_session $session_name $session_path
+  _flogger -n zup -l debug "session_name: '$session_name' session_cwd: '$session_cwd'"
+
+  __zup_create_session $session_name $session_cwd
   __zup_attach_session $session_name
 end
 
@@ -21,21 +23,29 @@ function __zup_get_session_name
   basename $cwd
 end
 
-function __zup_create_session
-  set -l session_name $argv[1]
-  set -l session_path $argv[2]
+function __zup_create_session -a session_name session_cwd
+  _flogger -n zup -l debug "creating session if needed"
 
-  if ! tmux has-session -t $session_name
-    tmux new-session -d -s $session_name -c $session_cwd
+  if tmux has-session -t $session_name &> /dev/null
+    _flogger -n zup -l debug "session with name '$session_name' found, skipping"
+    return
   end
+
+  _flogger -n zup -l debug "session with name '$session_name' not found, creating"
+  tmux new-session -d -s $session_name -c $session_cwd
+  _flogger -n zup -l debug "session with name '$session_name' created"
 end
 
 function __zup_attach_session
   set -l session_name $argv[1]
 
+  _flogger -n zup -l debug "attaching to session '$session_name'"
+
   if test -z $TMUX
+    _flogger -n zup -l debug "not in tmux session, attaching"
     tmux attach-session -t $session_name
   else
+    _flogger -n zup -l debug "in tmux session, switching"
     tmux switch-client -t $session_name
   end
 end
