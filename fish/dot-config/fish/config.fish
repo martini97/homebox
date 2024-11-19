20029850a3018c8ffa3ecc239d5adf5c596ee9b4
Alessandro Martini <martini97@protonmail.ch>
2024-11-19 16:40:57 -0300

    chore(fish): add cargo bin to path

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,9 +1,10 @@
 fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
 
 set -gx EDITOR nvim
 
 alias hb="homebox"
 
 if status is-interactive && command --query zoxide
-  zoxide init fish | source
+    zoxide init fish | source
 end
