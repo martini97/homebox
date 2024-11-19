250b2d45d82ccbc4d6b51d8396877d9ea1ee89c3
Alessandro Martini <martini97@protonmail.ch>
2024-11-19 13:10:48 -0300

    feat(neovim/snippets): snippets!

--- neovim/dot-config/nvim/after/ftplugin/lua.lua
+++ neovim/dot-config/nvim/after/ftplugin/lua.lua
@@ -19,3 +19,6 @@ vim.lsp.start({
 		},
 	},
 })
+
+vim.snippet.add("fn", "function ${1:name}($2)\n\t${3:-- content}\nend", { buffer = 0 })
+vim.snippet.add("lfn", "local function ${1:name}($2)\n\t${3:-- content}\nend", { buffer = 0 })
