3cf1204d75567db5dff065ca8d5cd44b9ec80146
Alessandro Martini <martini97@protonmail.ch>
2024-11-22 12:04:40 -0300

    chore(neovim/snippets): awquniq snippet

--- neovim/dot-config/nvim/snippets/sh.lua
+++ neovim/dot-config/nvim/snippets/sh.lua
@@ -6,4 +6,5 @@ local t = ls.text_node
 return {
 	s("shebang", t({ "#!/usr/bin/env bash", "" })),
 	s("set", t({ "set -o errexit", "set -o nounset", "set -o pipefail", "" })),
+	s("awkuniq", t({ [[awk '!a[$0]++']] })),
 }
