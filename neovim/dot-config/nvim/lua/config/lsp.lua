68f339b8d95379c7363ccfc6c017ac9efbc587d9
Alessandro Martini <martini97@protonmail.ch>
2024-11-22 17:04:43 -0300

    chore(neovim): organize lsp stuff

--- neovim/dot-config/nvim/lua/config/lsp.lua
+++ neovim/dot-config/nvim/lua/config/lsp.lua
@@ -1,71 +1,16 @@
 local lsp_util = require("lspconfig.util")
-local lsp_core = require("core.lsp.completion")
-local lsp_helpers = require("core.lsp.helpers")
-
-local methods = vim.lsp.protocol.Methods
-
----@param ev { buf: integer, data: { client_id: integer } }
-local function lsp_on_attach(ev)
-	---@param lhs string
-	---@param rhs string|function
-	---@param opts? string|vim.keymap.set.Opts
-	---@param mode? string|string[]
-	local map = function(lhs, rhs, opts, mode)
-		opts = type(opts) == "string" and { desc = opts } or opts or {}
-		opts = vim.tbl_deep_extend("error", opts --[[@as vim.keymap.set.Opts]], { buffer = ev.buf })
-		mode = mode or "n"
-		vim.keymap.set(mode, lhs, rhs, opts)
-	end
-
-	---@param fn fun(bufnr: integer)
-	local buf_call = function(fn)
-		return function()
-			return fn(ev.buf)
-		end
-	end
-
-	vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-	map("gd", vim.lsp.buf.definition, "[lsp] definition")
-	map("gro", vim.lsp.buf.outgoing_calls, "[lsp] outgoing calls")
-
-	local client = vim.lsp.get_client_by_id(ev.data.client_id)
-	if not client then
-		return
-	end
-
-	if client:supports_method(methods.textDocument_inlayHint, ev.buf) then
-		map("<leader>th", buf_call(lsp_helpers.toggle_inlay_hints), "[lsp] toggle inlay hints")
-	end
-
-	if client:supports_method(methods.textDocument_codeLens, ev.buf) then
-		map("<leader>tl", buf_call(lsp_helpers.toggle_inlay_hints), "[lsp] toggle code lens")
-	end
-
-	if client:supports_method(methods.textDocument_completion, ev.buf) then
-		lsp_core.enable_completion(client, ev.buf)
-
-		if client:supports_method(methods.completionItem_resolve, ev.buf) then
-			lsp_core.enable_completion_docs(client, ev.buf)
-		end
-	end
-end
-
----@param config? lspconfig.Config
----@return lspconfig.Config
-local function lsp_with_caps(config)
-	config = config or {}
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, config.capabilities or {})
-	return config
-end
+local lsp = require("core.lsp")
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
-	callback = lsp_on_attach,
+	callback = lsp.attach.on_attach,
 })
 
 local servers = {
+	pyright = {},
+	eslint = {},
+	bashls = {},
+	volar = { root_dir = lsp_util.root_pattern("vue.config.js", "package.json", ".git") },
 	lua_ls = {
 		settings = {
 			Lua = {
@@ -75,7 +20,6 @@ local servers = {
 			},
 		},
 	},
-	pyright = {},
 	vtsls = {
 		settings = {
 			typescript = { updateImportsOnFileMove = "always" },
@@ -87,11 +31,8 @@ local servers = {
 			},
 		},
 	},
-	eslint = {},
-	volar = { root_dir = lsp_util.root_pattern("vue.config.js", "package.json", ".git") },
-	bashls = {},
 }
 
 for name, config in pairs(servers) do
-	require("lspconfig")[name].setup(lsp_with_caps(config))
+	require("lspconfig")[name].setup(lsp.caps.with_caps(config))
 end
---
+++ neovim/dot-config/nvim/lua/core/lsp/init.lua
@@ -0,0 +1,4 @@
+return {
+	attach = require("core.lsp.attach"),
+	caps = require("core.lsp.capabilities"),
+}
