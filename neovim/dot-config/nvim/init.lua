7f3b502173e194647796bd2b62377c5f60a506cb
Alessandro Martini <18130319+martini97@users.noreply.github.com>
2025-03-12 12:26:47 -0300

    chore(neovim): blink cmp

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -423,6 +423,26 @@ do --- lazy
 				ft = "http",
 			},
 			--- }}}
+			--- blink.nvim {{{
+			{
+				"saghen/blink.cmp",
+				version = "*",
+				dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+				opts = {
+					snippets = { preset = "luasnip" },
+					keymap = { preset = "default" },
+					appearance = {
+						use_nvim_cmp_as_default = true,
+						nerd_font_variant = "mono",
+					},
+					sources = {
+						default = { "lsp", "path", "snippets", "buffer" },
+					},
+					fuzzy = { implementation = "prefer_rust_with_warning" },
+				},
+				opts_extend = { "sources.default" },
+			},
+			--- }}}
 		},
 	})
 end
