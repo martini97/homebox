f2897a7923e962a83ff14bb36d0628548cb2ec74
Alessandro Martini <18130319+martini97@users.noreply.github.com>
2025-08-05 22:13:35 -0300

    chore: updates
    Signed-off-by: Alessandro Martini <18130319+martini97@users.noreply.github.com>

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -311,3 +311,14 @@ do -- status
 		end
 	})
 end
+
+do -- copilot
+	vim.pack.add({
+		{ src = "https://github.com/nvim-lua/plenary.nvim",         version = "master" },
+		{ src = "https://github.com/zbirenbaum/copilot.lua" },
+		{ src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim" },
+	})
+
+	require("copilot").setup({})
+	require("CopilotChat").setup({})
+end
